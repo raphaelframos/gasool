73ea2a4951d0b04c147bb52494d00f4e59a831aa
Raphael Ferreira Ramos <raphaelframos@gmail.com>
2021-03-03 20:39:35 -0300

    Interacao entre views

--- app/src/main/java/com/powellapps/gasool/MainActivity.kt
+++ app/src/main/java/com/powellapps/gasool/MainActivity.kt
@@ -2,12 +2,24 @@ package com.powellapps.gasool
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatTextView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button : AppCompatButton = findViewById(R.id.button_calculate)
+        val textViewResult : AppCompatTextView = findViewById(R.id.text_view_result)
+
+        button.setOnClickListener {
+            Toast.makeText(this, "Colé parceiro", Toast.LENGTH_LONG).show()
+            textViewResult.text = "Tamo ai ${Date().time}"
+        }
 
     }
 }
